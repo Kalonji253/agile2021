f2a5f8b1774766e54454796304c95ff9e9027fd1
Kalonji253 <75808285+Kalonji253@users.noreply.github.com>
2021-12-03 00:31:45 +0000

    Add files via upload

---
+++ HelloPython.py
@@ -0,0 +1,15 @@
+import datetime
+print("Hello world!")
+now = datetime.datetime.now()
+print ("Current date and time is ")
+print (now.strftime("%A, %d-%m-%Y : %H:%M"))
+
+
+print("second change to see if the build would update upon changes made")
+
+
+print("Testing")
+
+print("Demo")
+
+print("Demo1")
